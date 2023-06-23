975a00488dd14d5ce204d3bcd72683a0c6eb83f8
jominuk <jominuk1025@naver.com>
2023-06-23 16:22:31 +0900

    :art: button

---
+++ src/page/Chat.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Chat = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <StGo onClick={() => navigate("/ai")}>Chat 바로가기</StGo>
+    </>
+  );
+};
+
+export default Chat;
+
+const StGo = styled.div`
+  background-color: pink;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 150px;
+  font-size: 20px;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  transition: 0.5s;
+  &:hover {
+    color: black;
+  }
+`;
--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "src/ui/Layout";
 import Ai from "src/page/Ai";
+import Chat from "src/page/Chat";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Ai />} />
+          <Route path="/ai" element={<Ai />} />
+          <Route path="/" element={<Chat />} />
         </Routes>
       </Layout>
     </BrowserRouter>
