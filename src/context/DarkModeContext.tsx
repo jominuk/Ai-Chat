77929e69a5e38dcf47975c1ebe568a75f804a124
jominuk <jominuk1025@naver.com>
2023-06-30 16:41:30 +0900

    :memo: 주석 설명

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -19,10 +19,12 @@ const DarkModeProvider: FC<DarkMode> = ({ children }) => {
   const storedDarkMode = localStorage.getItem("darkMode");
   const [darkMode, setDarkMode] = useState<boolean>(storedDarkMode === "true");
 
+  //다크모드 상태 유지를 위해 저장 위치를 만듬
   useEffect(() => {
     localStorage.setItem("darkMode", darkMode.toString());
   }, [darkMode]);
 
+  // 다크모드 버튼도 같이 context로
   const toggleDarkMode = () => {
     setDarkMode((prevMode) => !prevMode);
   };
